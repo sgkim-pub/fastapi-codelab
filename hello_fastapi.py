0c07e672cb0134137d6adbcd88aae95a622ef3b8
Seunggu Kim <wonears@gmail.com>
2025-07-22 07:17:38 +0900

    appending lifespan to app

--- hello_fastapi.py
+++ hello_fastapi.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from contextlib import asynccontextmanager
+
+# app = FastAPI()
+
+# 아래는 FastAPI의 생애주기(lifespan) 함수입니다.
+@asynccontextmanager
+async def lifespan(app):
+    print('Starting Hello, FastAPI.')
+    yield
+    print('Stopping Hello, FastAPI.')
+
+# 생애주기 함수를 이용하는 경우
+app = FastAPI(lifespan=lifespan)
 
 @app.get('/')
 def sayHello():
@@ -9,4 +21,5 @@ def sayHello():
 import uvicorn
 
 if __name__ == '__main__':
+    print('FastAPI app started.')
     uvicorn.run('hello_fastapi:app', host='0.0.0.0', reload=False, port=8000)
