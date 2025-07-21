9a8d20b512e477e543424d27c61173a0a4e8a0fa
Seunggu Kim <wonears@gmail.com>
2025-07-22 08:20:12 +0900

    sync task and async task

---
+++ sync_async.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+import time
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+def syncTask(name):
+    print('Sync task:', name)
+    time.sleep(1)
+    return name
+
+@app.get('/synctask')
+def syncRun():
+    now = datetime.now()
+    results = [syncTask('A'), syncTask('B'), syncTask('C')]
+    print('syncRun() running time:', datetime.now() - now)
+
+    return {"result": results}
+
+import asyncio
+
+async def asyncTask(name):
+    print('Async task:', name)
+    await asyncio.sleep(1)
+    return name
+
+@app.get('/asynctask')
+async def asyncRun():
+    now = datetime.now()
+    results = await asyncio.gather(asyncTask('A'), asyncTask('B'), asyncTask('C'))
+    print('asyncRun() running time:', datetime.now() - now)
+
+    return {"result": results}
+
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run("sync_async:app", host="0.0.0.0", reload=False, port=8000)
