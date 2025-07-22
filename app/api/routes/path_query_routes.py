a28f04f95a96ced664e4cccff031b6d815476d00
Seunggu Kim <wonears@gmail.com>
2025-07-22 15:28:51 +0900

    add query string method to receive data

--- app/api/routes/path_query_routes.py
+++ app/api/routes/path_query_routes.py
@@ -29,3 +29,40 @@ def getPathParam(itemId: Annotated[str, Path(...)]):
         content=respJSON
         , status_code=200
     )
+
+# # query string 방식으로 데이터를 받는 경우
+# from fastapi import Query
+
+# @pathQueryRouter.get('/product')
+# def getQueryStrings(
+#     itemCategory: str = Query(None)
+#     , itemId: str = Query(...)
+# ):
+#     print('path_query_routes.py.getQueryStrings().itemCategory:', itemCategory)
+#     print('path_query_routes.py.getQueryStrings().itemId:', itemId)
+
+#     respJSON = {"itemCategory": itemCategory, "itemId": itemId}
+
+#     return JSONResponse(
+#         content=respJSON
+#         , status_code=200
+#     )
+
+# query string 방식으로 데이터를 받는 경우 - 파이썬 Annotated 이용
+from fastapi import Query
+
+@pathQueryRouter.get('/product')
+def getQueryStrings(
+    itemId: Annotated[str, Query()]
+    , itemCategory: Annotated[str|None, Query()] = None
+):
+    print('path_query_routes.py.getQueryStrings().itemCategory:', itemCategory)
+    print('path_query_routes.py.getQueryStrings().itemId:', itemId)
+
+    respJSON = {"itemCategory": itemCategory, "itemId": itemId}
+
+    return JSONResponse(
+        content=respJSON
+        , status_code=200
+    )
+
