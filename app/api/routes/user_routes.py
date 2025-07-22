41bcde7cac2e15bb4f04b9e96264bdc5e9eeff05
Seunggu Kim <wonears@gmail.com>
2025-07-22 16:19:37 +0900

    read data from request body which includes file

--- app/api/routes/user_routes.py
+++ app/api/routes/user_routes.py
@@ -52,32 +52,73 @@ def sendSignupPage():
 #     )
 
 
-# 요청 바디에 포함된 JSON을 읽고 Pydantic 라이브러리로 검증한다.
-from pydantic import BaseModel, Field
-from fastapi.responses import JSONResponse
-
-class SignupInfo(BaseModel):
-    username: str
-    password: str
-
-# from typing import Annotated
+# # 요청 바디에 포함된 JSON을 읽고 Pydantic 라이브러리로 검증한다.
+# from pydantic import BaseModel, Field
+# from fastapi.responses import JSONResponse
 
 # class SignupInfo(BaseModel):
 #     username: str
-#     password: Annotated[str, Field(min_length=5)]
+#     password: str
+
+# # from typing import Annotated
+
+# # class SignupInfo(BaseModel):
+# #     username: str
+# #     password: Annotated[str, Field(min_length=5)]
+
+# @userRouter.post('/signup', status_code=201)
+# def registerUser(signupinfo: SignupInfo):
+#     username = signupinfo.username
+#     password = signupinfo.password
+
+#     respJSON = {"username": username, "password": password}
+
+#     print('user_routes.py.registerUser().username(Pydantic):', username)
+#     print('user_routes.py.registerUser().password(Pydantic):', password)
+
+#     return JSONResponse(
+#         content=respJSON
+#         , status_code=201
+#     )
+
+# 파일이 포함된 요청의 바디를 읽는 경우
+from fastapi import UploadFile, Form, File
+from secrets import token_hex
+import os
+from fastapi.responses import JSONResponse
 
 @userRouter.post('/signup', status_code=201)
-def registerUser(signupinfo: SignupInfo):
-    username = signupinfo.username
-    password = signupinfo.password
+async def registerUser(
+    username: str = Form(...)
+    , password: str = Form(...)
+    , picture: UploadFile = File(None)
+):
+    if picture:
+        pictureFileName = picture.filename
 
-    respJSON = {"username": username, "password": password}
+        fn = token_hex(10)
+        ext = os.path.splitext(pictureFileName)[1]
+        newPicFileName = fn + ext
 
-    print('user_routes.py.registerUser().username(Pydantic):', username)
-    print('user_routes.py.registerUser().password(Pydantic):', password)
+        savePath = os.path.join('app/upload', 'user_profile')
+        os.makedirs(savePath, exist_ok=True)
+
+        filePath = os.path.join(savePath, newPicFileName)
+        print('user_routes.py.registerUser().filePath:', filePath)
+
+        with open(filePath, 'wb') as buffer:
+            buffer.write(await picture.read())
+    else:
+        pictureFileName = None
+        newPicFileName = None
+
+    respJSON = {"username": username, "password": password, "picture": pictureFileName}
+
+    print('user_routes.py.registerUser().username:', username)
+    print('user_routes.py.registerUser().password:', password)
+    print('user_routes.py.registerUser().pictureFileName:', pictureFileName)
 
     return JSONResponse(
         content=respJSON
         , status_code=201
     )
-
